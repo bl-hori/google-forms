f53a6cd563dd3dbdd57ec5f1a104c25bb6c89fb4
Naoto Hori <naoto.hori@gmail.com>
2023-09-12 03:59:23 +0900

    add default value to career_form

--- forms/views.py
+++ forms/views.py
@@ -11,6 +11,14 @@ class CareerFormView(FormView):
     form_class = CareerForm
     success_url = reverse_lazy('forms.career_form')
 
+    def get_initial(self):
+        form_value = super().get_initial()
+        user = self.request.user
+        if user.id:
+            form_value.update(vars(user.careerformmodel_set.last()))
+
+        return form_value
+
     def form_valid(self, form):
         cleaned_data = form.cleaned_data
         career_form = CareerFormModel(**cleaned_data)
